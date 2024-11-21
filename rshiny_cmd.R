42d6ff3ee8f1f3397cf89f35ae43dc8a42023619
haider al-rustem <halrustem@Blakes-iMac.local>
2024-11-21 15:55:40 -0600

    update

--- rshiny_cmd.R
+++ rshiny_cmd.R
@@ -1,4 +1,4 @@
-#setwd("/root/public-dashboard");
-setwd("/app/secondTEPHIDemo/public-dashboard");
-shiny::runApp('/app/secondTEPHIDemo/public-dashboard', port = 9100)
+
+setwd("/app/public_SeqBoard");
+shiny::runApp('/app/public_SeqBoard', port = 9100)
 
